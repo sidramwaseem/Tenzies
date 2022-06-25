55882005b32ff22630c2e1f761ddc60f019198a8
sidramwaseem <sidraw24@gmail.com>
2022-06-25 22:32:22 +0500

    format index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App.js";
-import "./scss/style.css" ;
+import "./scss/style.css";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
