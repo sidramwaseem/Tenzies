f176a8ae32bf6521bb947619ee6aa3d9e074c83c
sidramwaseem <sidraw24@gmail.com>
2022-06-25 15:21:53 +0500

    added dice instances

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Die from "./components/Die.jsx";
 
 function App() {
   return (
     <main class="main">
-      <h1>Hello World!</h1>
+      <div className="main__dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
