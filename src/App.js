a1759c9e5f8f3fd95b6d2cfbfa9615b7dad584aa
sidramwaseem <sidraw24@gmail.com>
2022-06-25 19:28:45 +0500

    created neeDice function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import Die from "./components/Die.jsx";
 
 function App() {
+  function allNewDice(){
+    let diceNum = [];
+    for(let i= 0; i<10; i++){
+      diceNum.push(Math.ceil(Math.random() * 6));
+    }
+    return diceNum;
+  }
+
   return (
     <main class="main">
       <div className="main__dice-container">
