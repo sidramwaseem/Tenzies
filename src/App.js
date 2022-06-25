f31a717425b30832b6adcfcff4365ac334168c60
sidramwaseem <sidraw24@gmail.com>
2022-06-25 21:49:49 +0500

    Format app.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import React from "react";
 import Die from "./components/Die.jsx";
 
 function App() {
-
   const [dice, setDice] = React.useState(allNewDice());
 
-  function rollDice(){
+  function rollDice() {
     setDice(allNewDice());
   }
 
-  function allNewDice(){
+  function allNewDice() {
     let diceNum = [];
-    for(let i= 0; i<10; i++){
+    for (let i = 0; i < 10; i++) {
       diceNum.push(Math.ceil(Math.random() * 6));
     }
     return diceNum;
   }
 
-  const diceElement = dice.map(die=> <Die value={die} />)
+  const diceElement = dice.map((die) => <Die value={die} />);
 
   return (
     <main className="main">
-      <div className="main__dice-container">
-       {diceElement}
-      </div>
-      <button onClick={rollDice} className="main__btn" >Roll</button>
+      <div className="main__dice-container">{diceElement}</div>
+      <button onClick={rollDice} className="main__btn">
+        Roll
+      </button>
     </main>
   );
 }
