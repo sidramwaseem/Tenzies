f5042565c5a8abd25391d8e1d8af3c64227b66f8
sidramwaseem <sidraw24@gmail.com>
2022-06-25 19:53:12 +0500

    added state to dynamically assign dice numbers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Die from "./components/Die.jsx";
 
 function App() {
+
+  const [dice, setDice] = React.useState(allNewDice());
+
+
   function allNewDice(){
     let diceNum = [];
     for(let i= 0; i<10; i++){
@@ -10,19 +14,12 @@ function App() {
     return diceNum;
   }
 
+  const diceElement = dice.map(die=> <Die value={die} />)
+
   return (
     <main class="main">
       <div className="main__dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
+       {diceElement}
       </div>
     </main>
   );
