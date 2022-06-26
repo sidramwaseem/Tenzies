62ebe0c1488f5ebba505a196ffbf6cc364eabadc
sidramwaseem <sidraw24@gmail.com>
2022-06-26 14:36:09 +0500

    Added styles for isHeld State

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 function Die(props) {
+  const styles = {
+    backgroundColor : props.isHeld ? "#59E391" : "#FFFFFF",
+  }
   return (
-    <div className="die">
+    <div className="die" style={styles} >
       <h3 className="die__number">{props.value}</h3>
     </div>
   );
