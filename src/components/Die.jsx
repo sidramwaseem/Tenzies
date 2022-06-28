7c5c108adeb0a7678bd650e9204fdff391ba7417
sidramwaseem <sidraw24@gmail.com>
2022-06-28 22:34:12 +0500

    Update Die.jsx

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Die(props) {
+  // if(props.value === 1){
+  //   return <div>sup</div>
+  // }
+
   const styles = {
-    backgroundColor : props.isHeld ? "#59E391" : "#FFFFFF",
-  }
+    backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF",
+  };
   return (
-    <div className="die" style={styles} onClick={props.holdDice} >
+    <div className="die" style={styles} onClick={props.holdDice}>
       <h3 className="die__number">{props.value}</h3>
     </div>
   );
